3415d087bf36b94968b6551ffaa6d9c4181d8574
Mikhail S. Pobolovets <styx.mp@gmail.com>
2013-12-20 15:06:08 +0300

    Morse module test
    Signed-off-by: Mikhail S. Pobolovets <styx.mp@gmail.com>

--- test/morse_test.exs
+++ test/morse_test.exs
@@ -1,7 +1,28 @@
 defmodule MorseTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
 
-  test "the truth" do
-    assert(true)
+  test "to_morse with valid chars (EN)" do
+    assert Morse.to_morse("Morse", "en") == "-- --- .-. ... ."
   end
+
+  test "to_morse with valid chars (EN)" do
+    assert Morse.to_morse("Морзе", "ru") == "-- --- .-. --.. ."
+  end
+
+  test "preserves bad chars if set to" do
+    assert Morse.to_morse("M&%#e", "en") == "-- & % # ."
+  end
+
+  test "removes bad chars if not set to preserve" do
+    assert Morse.to_morse("M&%#e", "en", true) == "--    ."
+  end
+
+  test "decodes EN string" do
+    assert Morse.from_morse("-- --- .-. ... .", "en") == "MORSE"
+  end
+
+  test "decodes RU string" do
+    assert Morse.from_morse("-- --- .-. --.. .", "ru") == "МОРЗЕ"
+  end
+
 end
